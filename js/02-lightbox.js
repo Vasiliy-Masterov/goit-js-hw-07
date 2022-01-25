f5da162fd02da26a25ef8038bc0d8368f0e1d513
Vasiliy-Masterov <88328749+Vasiliy-Masterov@users.noreply.github.com>
2022-01-25 02:19:41 +0200

    Add files via upload

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector("body .gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => 
+ `<a class="gallery__item" href='${original}'>
+      <img
+        class="gallery__image"
+        src='${preview}'
+        alt='${description}'
+      />
+  </a>`
+).join("");}
+
+/*
+
+<a class="gallery__item" href="large-image.jpg">
+    <img
+        class="gallery__image"
+        src="small-image.jpg"
+        alt="Image description" />
+</a>
+
+*/
